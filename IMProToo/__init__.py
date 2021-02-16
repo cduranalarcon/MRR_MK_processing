1e8e72eaf9825d0086bfa95cadda321efd0165ba
Claudio Duran-Alarcon <44102364+cduranalarcon@users.noreply.github.com>
2021-02-16 10:46:08 +0100

    Add files via upload

---
+++ IMProToo/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+IMProToo
+Improved MRR Processing Tool
+"""
+from __future__ import division
+
+
+from .core2 import *
+from .core2 import __version__
+from .tools2 import *
